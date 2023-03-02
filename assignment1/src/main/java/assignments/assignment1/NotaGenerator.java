9b4794788a86ee4c3364d058b2c72cd0e795432f
Pradipta Arya Pramudita <diptaarya18@gmail.com>
2023-03-02 21:25:45 +0700

    Menambahkan command dan penjelasan pada kode

--- assignment1/src/main/java/assignments/assignment1/NotaGenerator.java
+++ assignment1/src/main/java/assignments/assignment1/NotaGenerator.java
@@ -1,5 +1,13 @@
+//=======Tugas Pemrograman 1=======\\
+//Nama: Pradipta Arya Pramudita    \\
+//NPM: 2206083685                  \\
+//Kelas: DDP 2-F                   \\
+//Kode Asdos: JHN                  \\
+//=================================\\
+
 package assignments.assignment1;
 
+//Import yang diperlukan
 import java.util.Scanner;
 import java.util.Arrays;
 import java.time.LocalDate;
@@ -115,102 +123,107 @@ public class NotaGenerator {
 
     //Method untuk generateID
     public static String generateId(String nama, String nomorHP) {
-        int id = 7;
-        nama = nama.toLowerCase();
-        char[] asciiName = nama.toCharArray();
-        char[] asciiNumber = nomorHP.toCharArray();
-
+        int id = 7; //Set initial variable id ke nilai 7
+        nama = nama.toLowerCase(); //Mengubah string nama menjadi huruf kecil
+        char[] asciiName = nama.toCharArray(); //Mengubah string nama menjadi character array
+        char[] asciiNumber = nomorHP.toCharArray(); //Mengubah string nomorhp menjadi character array
+        //Loop untuk mengambil setiap character nama
         for (char c : asciiName) {
+            //Menghentikan loop jika character merupakan spasi
             if (Character.isWhitespace(c)) {
                 break;
+            //Mengubah character menjadi 7 jika character bukan merupakan huruf
             } else if (!Character.isLetter(c)) {
                 c = '7';
+            //Mengubah character menjadi huruf kecil dan dikurangi a-1 jika character merupakan huruf
             } else {
                 c = Character.toLowerCase(c);
                 c -= 'a' - 1;
             }
+            //Hasil ditambahkan ke id
             id += c;
         }
-
+        //Loop untuk nomor hp
         for (char c : asciiNumber) {
+            //Jika character bukan merupakan digit maka diubah menjadi 7
             if (!Character.isDigit(c)) {
                 c = '7';
+            //Jika character merupakan digit maka character dikurangi dengan '0'
             } else {
                 c -= '0';
             }
+            //Menambahkan hasil ke ID
             id += c;
         }
-
+        //Konversi variable id menjadi string dengan format 2 digit
         String strId = String.format("%02d", id);
 
+        //Mengambil nama index 0 dan menyimpan ke variable finalName
         String finalName = nama.split(" ")[0];
+        //Mengubah nama akhir menjadi kapital dan menambahkan nomor serta id
         String result = finalName.toUpperCase() + "-" + nomorHP + "-" + strId;
-
+        //Mengembalikan variable result
         return result;
     }
 
-
-    /**
-     *
-     * Method untuk membuat Nota.
-     * Parameter dan return type dari method ini tidak boleh diganti agar tidak mengganggu testing.
-     *
-     * @return string nota dengan format di bawah:
-     *         <p>ID    : [id]
-     *         <p>Paket : [paket]
-     *         <p>Harga :
-     *         <p>[berat] kg x [hargaPaketPerKg] = [totalHarga]
-     *         <p>Tanggal Terima  : [tanggalTerima]
-     *         <p>Tanggal Selesai : [tanggalTerima + LamaHariPaket]
-     */
-
-     public static String generateNota(String id, String paket, int berat, String tanggalTerima){
+    //Method untuk generate nota
+    public static String generateNota(String id, String paket, int berat, String tanggalTerima){
+        //Format tanggal ke hari/bulan/tahun
         DateTimeFormatter formatTanggal = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        //Localdate dengan format yang ditentukan
         LocalDate tanggalTerimaObj = LocalDate.parse(tanggalTerima, formatTanggal);
+        //Assign variable integer
         int hargaLaundry;
         int hariSelesai;
-    
+        //Switch statement untuk set harga dan hari mengerjakan sesuai input paket yang ditentukan
         switch(paket.toLowerCase()){
-            case "express":
+            case "express": //Case input express
                 hargaLaundry = 12000;
                 hariSelesai = 1;
-                break;
-            case "fast":
+                break; 
+            case "fast": //Case input fast
                 hargaLaundry = 10000;
                 hariSelesai = 2;
                 break;
-            case "reguler":
+            case "reguler": //Case input reguler
                 hargaLaundry = 7000;
                 hariSelesai = 3;
                 break;
-            default:
+            default: //Mengalihkan input lain dan mencetak pesan gagal
                 throw new IllegalArgumentException("Paket " + paket + " tidak diketahui");
         }
-    
+        //Menambahkan jumlah hari ke LocalDate tanggal sebelumnya
         LocalDate tanggalSelesaiObj = tanggalTerimaObj.plusDays(hariSelesai);
+        //Set format tanggal selesai ke format tanggal yang sudah ditentukan
         String tanggalSelesai = tanggalSelesaiObj.format(formatTanggal);
+
+        //Menghitung total harga dan assign ke variable totalHarga
         int totalHarga = hargaLaundry * berat;
-    
+        
+        //Formatting output dengan string format
         return String.format("ID    : %s\nPaket : %s\nHarga :\n%d kg x %d = %d\nTanggal Terima  : %s\nTanggal Selesai : %s",
                 id, paket, berat, hargaLaundry, totalHarga, tanggalTerima, tanggalSelesai);
     }
     
-    
+    //Method untuk memisahkan nama depan
     public static String getFirstName() {
-        System.out.print("Masukkan nama Anda: ");
+        System.out.print("Masukkan nama Anda: "); //Meminta input nama
         String nama = input.nextLine();
-        String[] firstName = nama.split(" ");
-        return firstName[0];
+        String[] firstName = nama.split(" "); //Split input nama
+        return firstName[0]; //Mengembalikan nama pada index ke-0
     }
+
+    //Method untuk numCheck nomor hp
     public static String getNumber() {
-        System.out.print("Masukkan nomor handphone Anda: ");
+        System.out.print("Masukkan nomor handphone Anda: "); //Meminta input nomor hp
         String nomorhp = input.nextLine();
-        boolean numCheck = false;
+        boolean numCheck = false; //Boolean untuk loop
 
         while (!numCheck) {
+            //If condition untuk input angka
             if (nomorhp.matches("[0-9]+")) {
-                numCheck = true;
-                
+                numCheck = true; //Mengganti boolean numCheck menjadi true
+            //Else condition apabila input selain angka
             } else {
                 System.out.println("Nomor hp hanya menerima digit");
             }
