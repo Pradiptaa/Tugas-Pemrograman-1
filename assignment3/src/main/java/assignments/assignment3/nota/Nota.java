9d72e7522c349e06f26e14a7629767968b5ef188
Pradipta Arya Pramudita <diptaarya18@gmail.com>
2023-05-07 23:54:50 +0700

    iya

--- assignment3/src/main/java/assignments/assignment3/nota/Nota.java
+++ assignment3/src/main/java/assignments/assignment3/nota/Nota.java
@@ -30,7 +30,6 @@ public class Nota {
         this.isDone = false;
         this.id = totalNota++;
         this.services= new LaundryService[0];
-        tanggalSelesai = setTanggalSelesai();
 
         this.addService(new CuciService()); //Menambah cuci service
 
@@ -45,6 +44,7 @@ public class Nota {
             this.sisaHariPengerjaan = 3;
             this.baseHarga = 7000;
         }
+        tanggalSelesai = setTanggalSelesai();
 
         Calendar copyOfCal = Calendar.getInstance();
         copyOfCal.setTime (NotaManager.cal.getTime());
